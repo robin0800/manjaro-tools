6599ed15920c186c333d68cd2194e64aa0bc8482
udeved <artoo@manjaro.org>
2015-02-13 00:00:08 +0100

    [util-livecd] don't use lightdm autologin timeout

--- lib/util-livecd.sh
+++ lib/util-livecd.sh
@@ -319,7 +319,7 @@ configure_alsa_live(){
 configure_displaymanager_live(){
     if [[ -f /usr/bin/lightdm ]];then
 	gpasswd -a ${username} autologin &> /dev/null
-	sed -i -e 's/^.*autologin-user-timeout=.*/autologin-user-timeout=0/' /etc/lightdm/lightdm.conf
+# 	sed -i -e 's/^.*autologin-user-timeout=.*/autologin-user-timeout=0/' /etc/lightdm/lightdm.conf
 	sed -i -e "s/^.*autologin-user=.*/autologin-user=${username}/" /etc/lightdm/lightdm.conf
     elif [[ -f /usr/bin/kdm ]];then
 	sed -i -e "s/^.*AutoLoginUser=.*/AutoLoginUser=${username}/" /usr/share/config/kdm/kdmrc
