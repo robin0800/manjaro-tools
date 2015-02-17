4a031bc0ceac40d51513f068579d45675edf3464
udeved <artoo@manjaro.org>
2015-02-17 23:49:58 +0100

    [util-livecd] add the shared cli installer language functions

--- lib/util-livecd.sh
+++ lib/util-livecd.sh
@@ -11,18 +11,298 @@
 
 # this util-livecd.sh gets copied to overlay-image/opt/livecd
 
-check_ping(){
-	echo $(LC_ALL=C ping -c 1 www.manjaro.org | grep "1 received")
-}
-
-configure_machine_id_live(){
+configure_machine_id(){
 	# set unique machine-id
 	echo "Setting machine-id ..." >> /tmp/livecd.log
 	dbus-uuidgen --ensure=/etc/machine-id
 	ln -sf /etc/machine-id /var/lib/dbus/machine-id
 }
 
-configure_translation_pkgs_live(){
+install_localization(){
+	if [ -e "/bootmnt/${install_dir}/${arch}/lng-image.sqfs" ] ; then
+		echo "install translation packages" >> /tmp/livecd.log
+		configure_translation_pkgs
+		${PACMAN_LNG} -Sy
+		if [ -e "/bootmnt/${install_dir}/${arch}/kde-image.sqfs" ] ; then
+			${PACMAN_LNG} -S ${KDE_LNG_INST} &> /dev/null
+		fi
+		if [ -e "/usr/bin/firefox" ] ; then
+			${PACMAN_LNG} -S ${FIREFOX_LNG_INST} &> /dev/null
+		fi
+		if [ -e "/usr/bin/thunderbird" ] ; then
+			${PACMAN_LNG} -S ${THUNDER_LNG_INST} &> /dev/null
+		fi
+		if [ -e "/usr/bin/libreoffice" ] ; then
+			${PACMAN_LNG} -S ${LIBRE_LNG_INST} &> /dev/null
+		fi
+		if [ -e "/usr/bin/hunspell" ] ; then
+			${PACMAN_LNG} -S ${HUNSPELL_LNG_INST} &> /dev/null
+		fi
+	fi
+}
+
+configure_swap(){
+	local swapdev="$(fdisk -l 2>/dev/null | grep swap | cut -d' ' -f1)"
+	if [ -e "${swapdev}" ]; then
+		swapon ${swapdev}
+		echo "${swapdev} swap swap defaults 0 0 #configured by manjaro-tools" >>/etc/fstab
+	fi
+}
+
+# TODO: review sudoers
+configure_sudo(){
+	chown root:root /etc/sudoers
+	sed -i -e 's|# %wheel ALL=(ALL) ALL|%wheel ALL=(ALL) ALL|g' /etc/sudoers
+	sed -e 's|# root ALL=(ALL) ALL|root ALL=(ALL) ALL|' -i /etc/sudoers
+	echo "${username} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+	chmod 440 /etc/sudoers
+}
+
+configure_env(){
+	echo "BROWSER=/usr/bin/xdg-open" >> /etc/environment
+	echo "BROWSER=/usr/bin/xdg-open" >> /etc/skel/.bashrc
+	echo "BROWSER=/usr/bin/xdg-open" >> /etc/profile
+
+	# add TERM var
+
+	if [ -e "/usr/bin/mate-session" ] ; then
+	echo "TERM=mate-terminal" >> /etc/environment
+	echo "TERM=mate-terminal" >> /etc/profile
+	fi
+
+	## FIXME - Workaround to launch mate-terminal
+	if [ -e "/usr/bin/mate-session" ] ; then
+	sed -i -e "s~^.*Exec=.*~Exec=mate-terminal -e 'sudo setup'~" "/etc/skel/Desktop/installer-launcher-cli.desktop"
+	sed -i -e "s~^.*Terminal=.*~Terminal=false~" "/etc/skel/Desktop/installer-launcher-cli.desktop"
+	fi
+
+	if [[ -f "/bootmnt/${install_dir}/${arch}/${custom}-image.sqfs" ]]; then
+		case ${custom} in
+			gnome|xfce|openbox|enlightenment|cinnamon|pekwm|lxde|mate)
+				echo "QT_STYLE_OVERRIDE=gtk" >> /etc/environment
+			;;
+			*) break ;;
+		esac
+	fi
+}
+
+configure_user_root(){
+	# set up root password
+	echo "root:${password}" | chroot $1 chpasswd
+}
+
+configure_displaymanager(){
+	if [[ -f /usr/bin/lightdm ]];then
+		gpasswd -a ${username} autologin &> /dev/null
+# 		sed -i -e 's/^.*autologin-user-timeout=.*/autologin-user-timeout=0/' /etc/lightdm/lightdm.conf
+		sed -i -e "s/^.*autologin-user=.*/autologin-user=${username}/" /etc/lightdm/lightdm.conf
+		elif [[ -f /usr/bin/kdm ]];then
+		sed -i -e "s/^.*AutoLoginUser=.*/AutoLoginUser=${username}/" /usr/share/config/kdm/kdmrc
+		sed -i -e "s/^.*AutoLoginPass=.*/AutoLoginPass=${password}/" /usr/share/config/kdm/kdmrc
+		xdg-icon-resource forceupdate --theme hicolor &> /dev/null
+		[[ -e "/usr/bin/update-desktop-database" ]] && update-desktop-database -q
+		elif [[ -f /usr/bin/sddm ]];then
+		sed -i -e "s|^User=.*|User=${username}|" /etc/sddm.conf
+		elif [[ -f /usr/bin/lxdm ]];then
+		sed -i -e "s/^.*autologin=.*/autologin=${username}/" /etc/lxdm/lxdm.conf
+	fi
+}
+
+### shared functions with cli installer
+
+configure_alsa(){
+	echo "configure alsa" >> /tmp/livecd.log
+	# amixer binary
+	local alsa_amixer="chroot $1 /usr/bin/amixer"
+
+	# enable all known (tm) outputs
+	$alsa_amixer -c 0 sset "Master" 70% unmute &>/dev/null
+	$alsa_amixer -c 0 sset "Front" 70% unmute &>/dev/null
+	$alsa_amixer -c 0 sset "Side" 70% unmute &>/dev/null
+	$alsa_amixer -c 0 sset "Surround" 70% unmute &>/dev/null
+	$alsa_amixer -c 0 sset "Center" 70% unmute &>/dev/null
+	$alsa_amixer -c 0 sset "LFE" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "Headphone" 70% unmute &>/dev/null
+	$alsa_amixer -c 0 sset "Speaker" 70% unmute &>/dev/null
+	$alsa_amixer -c 0 sset "PCM" 70% unmute &>/dev/null
+	$alsa_amixer -c 0 sset "Line" 70% unmute &>/dev/null
+	$alsa_amixer -c 0 sset "External" 70% unmute &>/dev/null
+	$alsa_amixer -c 0 sset "FM" 50% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "Master Mono" 70% unmute &>/dev/null
+	$alsa_amixer -c 0 sset "Master Digital" 70% unmute &>/dev/null
+	$alsa_amixer -c 0 sset "Analog Mix" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "Aux" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "Aux2" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "PCM Center" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "PCM Front" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "PCM LFE" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "PCM Side" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "PCM Surround" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "Playback" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "PCM,1" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "DAC" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "DAC,0" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "DAC,0" -12dB &> /dev/null
+	$alsa_amixer -c 0 sset "DAC,1" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "DAC,1" -12dB &> /dev/null
+	$alsa_amixer -c 0 sset "Synth" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "CD" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "Wave" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "Music" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "AC97" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "Analog Front" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "VIA DXS,0" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "VIA DXS,1" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "VIA DXS,2" 70% unmute &> /dev/null
+	$alsa_amixer -c 0 sset "VIA DXS,3" 70% unmute &> /dev/null
+
+	# set input levels
+	$alsa_amixer -c 0 sset "Mic" 70% mute &>/dev/null
+	$alsa_amixer -c 0 sset "IEC958" 70% mute &>/dev/null
+
+	# special stuff
+	$alsa_amixer -c 0 sset "Master Playback Switch" on &>/dev/null
+	$alsa_amixer -c 0 sset "Master Surround" on &>/dev/null
+	$alsa_amixer -c 0 sset "SB Live Analog/Digital Output Jack" off &>/dev/null
+	$alsa_amixer -c 0 sset "Audigy Analog/Digital Output Jack" off &>/dev/null
+}
+
+check_ping(){
+	echo $(LC_ALL=C ping -c 1 www.manjaro.org | grep "1 received")
+}
+
+kernel_cmdline(){
+	for param in $(/bin/cat /proc/cmdline); do
+		case "${param}" in
+			$1=*) echo "${param##*=}"; return 0 ;;
+			$1) return 0 ;;
+			*) continue ;;
+		esac
+	done
+	[ -n "${2}" ] && echo "${2}"
+	return 1
+}
+
+get_country(){
+  local COUNTRY=$(kernel_cmdline lang)
+  echo $COUNTRY
+}
+
+get_keyboard(){
+  local KEYBOARD=$(kernel_cmdline keytable)
+  echo $KEYBOARD
+}
+
+get_layout(){
+  local LAYOUT=$(kernel_cmdline layout)
+  echo $LAYOUT
+}
+
+find_legacy_keymap(){
+  file="/opt/livecd/kbd-model-map"
+  while read -r line || [[ -n $line ]]; do
+    if [[ -z $line ]] || [[ $line == \#* ]]; then
+      continue
+    fi
+
+    mapping=( $line ); # parses columns
+    if [[ ${#mapping[@]} != 5 ]]; then
+      continue
+    fi
+
+    if  [[ "$KEYMAP" != "${mapping[0]}" ]]; then
+      continue
+    fi
+
+    if [[ "${mapping[3]}" = "-" ]]; then
+      mapping[3]=""
+    fi
+
+    X11_LAYOUT=${mapping[1]}
+    X11_MODEL=${mapping[2]}
+    X11_VARIANT=${mapping[3]}
+    x11_OPTIONS=${mapping[4]}
+  done < $file
+}
+
+write_x11_config(){
+  # find a x11 layout that matches the keymap
+  # in isolinux if you select a keyboard layout and a language that doesnt match this layout,
+  # it will provide the correct keymap, but not kblayout value
+  local X11_LAYOUT=
+  local X11_MODEL="pc105"
+  local X11_VARIANT=""
+  local X11_OPTIONS="terminate:ctrl_alt_bksp"
+  find_legacy_keymap
+
+  # layout not found, use KBLAYOUT
+  if [[ -z "$X11_LAYOUT" ]]; then
+    X11_LAYOUT="$KBLAYOUT"
+  fi
+
+  # create X11 keyboard layout config
+  mkdir -p "${DESTDIR}/etc/X11/xorg.conf.d"
+  if [ -e /run/openrc ]; then
+    local XORGKBLAYOUT="${DESTDIR}/etc/X11/xorg.conf.d/20-keyboard.conf"
+  else
+    local XORGKBLAYOUT="${DESTDIR}/etc/X11/xorg.conf.d/00-keyboard.conf"
+  fi
+
+  echo "" >> "$XORGKBLAYOUT"
+  echo "Section \"InputClass\"" > "$XORGKBLAYOUT"
+  echo " Identifier \"system-keyboard\"" >> "$XORGKBLAYOUT"
+  echo " MatchIsKeyboard \"on\"" >> "$XORGKBLAYOUT"
+  echo " Option \"XkbLayout\" \"$X11_LAYOUT\"" >> "$XORGKBLAYOUT"
+  echo " Option \"XkbModel\" \"$X11_MODEL\"" >> "$XORGKBLAYOUT"
+  echo " Option \"XkbVariant\" \"$X11_VARIANT\"" >> "$XORGKBLAYOUT"
+  echo " Option \"XkbOptions\" \"$X11_OPTIONS\"" >> "$XORGKBLAYOUT"
+  echo "EndSection" >> "$XORGKBLAYOUT"
+
+  # fix por keyboardctl
+  if [ -f "${DESTDIR}/etc/keyboard.conf" ]; then
+    sed -i -e "s/^XKBLAYOUT=.*/XKBLAYOUT=\"${X11_LAYOUT}\"/g" ${DESTDIR}/etc/keyboard.conf
+  fi
+}
+
+configure_language(){
+	# hack to be able to set the locale on bootup
+	local LOCALE=$(get_country)
+	local KEYMAP=$(get_keyboard)
+	local KBLAYOUT=$(get_layout)
+
+	# set a default value, in case something goes wrong, or a language doesn't have
+	# good defult settings
+	[ -n "$LOCALE" ] || LOCALE="en_US"
+	[ -n "$KEYMAP" ] || KEYMAP="us"
+	[ -n "$KBLAYOUT" ] || KBLAYOUT="us"
+
+	# set keymap
+	if [ -e /run/openrc ]; then
+		sed -i "s/keymap=.*/keymap=\"${KEYMAP}\"/" ${DESTDIR}/etc/conf.d/keymaps
+	else
+		echo "KEYMAP=us" > ${DESTDIR}/etc/vconsole.conf
+		sed -i "s/^KEYMAP=.*/KEYMAP=\"${KEYMAP}\"/" ${DESTDIR}/etc/vconsole.conf
+	fi
+
+	# load keymaps
+	loadkeys "$KEYMAP"
+
+	write_x11_config
+
+	# set systemwide language
+	echo "LANG=${LOCALE}.UTF-8" > ${DESTDIR}/etc/locale.conf
+	echo "LC_MESSAGES=${LOCALE}.UTF-8" >> ${DESTDIR}/etc/locale.conf
+	echo "LANG=${LOCALE}.UTF-8" >> ${DESTDIR}/etc/environment
+
+	# generate LOCALE
+	local TLANG=${LOCALE%.*} # remove everything after the ., including the dot from LOCALE
+	sed -i -r "s/#(${TLANG}.*UTF-8)/\1/g" ${DESTDIR}/etc/locale.gen
+	# add also American English as safe default
+	sed -i -r "s/#(en_US.*UTF-8)/\1/g" ${DESTDIR}/etc/locale.gen
+
+}
+
+configure_translation_pkgs(){
 	# Determind which language we are using
 	local LNG_INST=$(cat $1/etc/locale.conf | grep LANG= | cut -d= -f2 | cut -d. -f1)
 	[ -n "$LNG_INST" ] || LNG_INST="en"
@@ -166,149 +446,9 @@ configure_translation_pkgs_live(){
 	esac
 }
 
-install_localization_live(){
-	if [ -e "/bootmnt/${install_dir}/${arch}/lng-image.sqfs" ] ; then
-		echo "install translation packages" >> /tmp/livecd.log
-		configure_translation_pkgs_live
-		${PACMAN_LNG} -Sy
-		if [ -e "/bootmnt/${install_dir}/${arch}/kde-image.sqfs" ] ; then
-			${PACMAN_LNG} -S ${KDE_LNG_INST} &> /dev/null
-		fi
-		if [ -e "/usr/bin/firefox" ] ; then
-			${PACMAN_LNG} -S ${FIREFOX_LNG_INST} &> /dev/null
-		fi
-		if [ -e "/usr/bin/thunderbird" ] ; then
-			${PACMAN_LNG} -S ${THUNDER_LNG_INST} &> /dev/null
-		fi
-		if [ -e "/usr/bin/libreoffice" ] ; then
-			${PACMAN_LNG} -S ${LIBRE_LNG_INST} &> /dev/null
-		fi
-		if [ -e "/usr/bin/hunspell" ] ; then
-			${PACMAN_LNG} -S ${HUNSPELL_LNG_INST} &> /dev/null
-		fi
-	fi
-}
-
-configure_swap_live(){
-	local swapdev="$(fdisk -l 2>/dev/null | grep swap | cut -d' ' -f1)"
-	if [ -e "${swapdev}" ]; then
-		swapon ${swapdev}
-		echo "${swapdev} swap swap defaults 0 0 #configured by manjaro-tools" >>/etc/fstab
-	fi
-}
-
-# TODO: review sudoers
-configure_sudo_live(){
-	chown root:root /etc/sudoers
-	sed -i -e 's|# %wheel ALL=(ALL) ALL|%wheel ALL=(ALL) ALL|g' /etc/sudoers
-	sed -e 's|# root ALL=(ALL) ALL|root ALL=(ALL) ALL|' -i /etc/sudoers
-	echo "${username} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-	chmod 440 /etc/sudoers
-}
-
-configure_env_live(){
-	echo "BROWSER=/usr/bin/xdg-open" >> /etc/environment
-	echo "BROWSER=/usr/bin/xdg-open" >> /etc/skel/.bashrc
-	echo "BROWSER=/usr/bin/xdg-open" >> /etc/profile
-
-	# add TERM var
-
-	if [ -e "/usr/bin/mate-session" ] ; then
-	echo "TERM=mate-terminal" >> /etc/environment
-	echo "TERM=mate-terminal" >> /etc/profile
-	fi
-
-	## FIXME - Workaround to launch mate-terminal
-	if [ -e "/usr/bin/mate-session" ] ; then
-	sed -i -e "s~^.*Exec=.*~Exec=mate-terminal -e 'sudo setup'~" "/etc/skel/Desktop/installer-launcher-cli.desktop"
-	sed -i -e "s~^.*Terminal=.*~Terminal=false~" "/etc/skel/Desktop/installer-launcher-cli.desktop"
-	fi
-
-	if [[ -f "/bootmnt/${install_dir}/${arch}/${custom}-image.sqfs" ]]; then
-		case ${custom} in
-			gnome|xfce|openbox|enlightenment|cinnamon|pekwm|lxde|mate)
-				echo "QT_STYLE_OVERRIDE=gtk" >> /etc/environment
-			;;
-			*) break ;;
-		esac
-	fi
-}
-
-configure_user_root_live(){
-	# set up root password
-	echo "root:${password}" | chroot $1 chpasswd
-}
-
-configure_alsa_live(){
-	echo "configure alsa" >> /tmp/livecd.log
-	# amixer binary
-	local alsa_amixer="chroot $1 /usr/bin/amixer"
-
-	# enable all known (tm) outputs
-	$alsa_amixer -c 0 sset "Master" 70% unmute &>/dev/null
-	$alsa_amixer -c 0 sset "Front" 70% unmute &>/dev/null
-	$alsa_amixer -c 0 sset "Side" 70% unmute &>/dev/null
-	$alsa_amixer -c 0 sset "Surround" 70% unmute &>/dev/null
-	$alsa_amixer -c 0 sset "Center" 70% unmute &>/dev/null
-	$alsa_amixer -c 0 sset "LFE" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "Headphone" 70% unmute &>/dev/null
-	$alsa_amixer -c 0 sset "Speaker" 70% unmute &>/dev/null
-	$alsa_amixer -c 0 sset "PCM" 70% unmute &>/dev/null
-	$alsa_amixer -c 0 sset "Line" 70% unmute &>/dev/null
-	$alsa_amixer -c 0 sset "External" 70% unmute &>/dev/null
-	$alsa_amixer -c 0 sset "FM" 50% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "Master Mono" 70% unmute &>/dev/null
-	$alsa_amixer -c 0 sset "Master Digital" 70% unmute &>/dev/null
-	$alsa_amixer -c 0 sset "Analog Mix" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "Aux" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "Aux2" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "PCM Center" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "PCM Front" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "PCM LFE" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "PCM Side" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "PCM Surround" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "Playback" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "PCM,1" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "DAC" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "DAC,0" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "DAC,0" -12dB &> /dev/null
-	$alsa_amixer -c 0 sset "DAC,1" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "DAC,1" -12dB &> /dev/null
-	$alsa_amixer -c 0 sset "Synth" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "CD" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "Wave" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "Music" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "AC97" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "Analog Front" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "VIA DXS,0" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "VIA DXS,1" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "VIA DXS,2" 70% unmute &> /dev/null
-	$alsa_amixer -c 0 sset "VIA DXS,3" 70% unmute &> /dev/null
-
-	# set input levels
-	$alsa_amixer -c 0 sset "Mic" 70% mute &>/dev/null
-	$alsa_amixer -c 0 sset "IEC958" 70% mute &>/dev/null
-
-	# special stuff
-	$alsa_amixer -c 0 sset "Master Playback Switch" on &>/dev/null
-	$alsa_amixer -c 0 sset "Master Surround" on &>/dev/null
-	$alsa_amixer -c 0 sset "SB Live Analog/Digital Output Jack" off &>/dev/null
-	$alsa_amixer -c 0 sset "Audigy Analog/Digital Output Jack" off &>/dev/null
-}
-
-configure_displaymanager_live(){
-	if [[ -f /usr/bin/lightdm ]];then
-		gpasswd -a ${username} autologin &> /dev/null
-# 		sed -i -e 's/^.*autologin-user-timeout=.*/autologin-user-timeout=0/' /etc/lightdm/lightdm.conf
-		sed -i -e "s/^.*autologin-user=.*/autologin-user=${username}/" /etc/lightdm/lightdm.conf
-		elif [[ -f /usr/bin/kdm ]];then
-		sed -i -e "s/^.*AutoLoginUser=.*/AutoLoginUser=${username}/" /usr/share/config/kdm/kdmrc
-		sed -i -e "s/^.*AutoLoginPass=.*/AutoLoginPass=${password}/" /usr/share/config/kdm/kdmrc
-		xdg-icon-resource forceupdate --theme hicolor &> /dev/null
-		[[ -e "/usr/bin/update-desktop-database" ]] && update-desktop-database -q
-		elif [[ -f /usr/bin/sddm ]];then
-		sed -i -e "s|^User=.*|User=${username}|" /etc/sddm.conf
-		elif [[ -f /usr/bin/lxdm ]];then
-		sed -i -e "s/^.*autologin=.*/autologin=${username}/" /etc/lxdm/lxdm.conf
-	fi
+printk(){
+    case ${1} in
+        "on")  echo 4 >/proc/sys/kernel/printk ;;
+        "off") echo 0 >/proc/sys/kernel/printk ;;
+    esac
 }
