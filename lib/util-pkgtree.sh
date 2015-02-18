cba53d391cbaa79531ff764247e8edfb68708ea8
udeved <artoo@manjaro.org>
2015-02-18 20:10:31 +0100

    [util-pkgtree] show clone and pull times

--- lib/util-pkgtree.sh
+++ lib/util-pkgtree.sh
@@ -11,7 +11,8 @@
 
 sync_tree(){
 	local master=$(git log --pretty=%H ...refs/heads/master^ | head -n 1) \
-		master_remote=$(git ls-remote origin -h refs/heads/master | cut -f1)
+		master_remote=$(git ls-remote origin -h refs/heads/master | cut -f1) \
+		timer=$(get_timer)
 	msg "Checking [$1] ..."
 	msg2 "local:  ${master}"
 	msg2 "remote: ${master_remote}"
@@ -21,41 +22,42 @@ sync_tree(){
 		msg3 "needs sync"
 		git pull origin master
 	fi
-	msg "Done [$1]"
+	msg "Done [$1] in $(elapsed_time ${timer}) minutes"
 }
 
 clone_tree(){
+	local timer=$(get_timer)
 	msg "Preparing [$1] ..."
 	msg3 "clone"
 	git clone $2.git
-	msg "Done [$1]"
+	msg "Done [$1] in $(elapsed_time ${timer}) minutes"
 }
 
 sync_tree_manjaro(){
 	cd ${tree_dir}
-	for repo in ${repo_tree[@]};do
-		if [[ -d packages-${repo} ]];then
-			cd packages-${repo}
-				sync_tree "${repo}"
-			cd ..
-		else
-			clone_tree "${repo}" "${host_tree}/packages-${repo}"
-		fi
-	done
+		for repo in ${repo_tree[@]};do
+			if [[ -d packages-${repo} ]];then
+				cd packages-${repo}
+					sync_tree "${repo}"
+				cd ..
+			else
+				clone_tree "${repo}" "${host_tree}/packages-${repo}"
+			fi
+		done
 	cd ..
 }
 
 sync_tree_abs(){
 	local repo_tree_abs=('packages' 'community')
 	cd ${tree_dir_abs}
-	for repo in ${repo_tree_abs[@]};do
-		if [[ -d ${repo} ]];then
-			cd ${repo}
-				sync_tree "${repo}"
-			cd ..
-		else
-			clone_tree "${repo}" "${host_tree_abs}/${repo}"
-		fi
-	done
+		for repo in ${repo_tree_abs[@]};do
+			if [[ -d ${repo} ]];then
+				cd ${repo}
+					sync_tree "${repo}"
+				cd ..
+			else
+				clone_tree "${repo}" "${host_tree_abs}/${repo}"
+			fi
+		done
 	cd ..
 }
