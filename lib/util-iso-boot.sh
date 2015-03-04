23788254aa15238c6de01b1794d4c869c66e1f3d
udeved <artoo@manjaro.org>
2015-03-04 13:26:06 +0100

    [util-iso-boot] fix isolinux binaries copy

--- lib/util-iso-boot.sh
+++ lib/util-iso-boot.sh
@@ -133,26 +133,26 @@ write_usb_nonfree_conf(){
 copy_isolinux_bin(){
 	msg2 "copying isolinux binaries ..."
 	if [[ -e $1/bios/ ]]; then
-		cp $1/usr/lib/syslinux/bios/isolinux.bin $2
-		cp $1/usr/lib/syslinux/bios/isohdpfx.bin $2
-		cp $1/usr/lib/syslinux/bios/ldlinux.c32 $2
-		cp $1/usr/lib/syslinux/bios/gfxboot.c32 $2
-		cp $1/usr/lib/syslinux/bios/whichsys.c32 $2
-		cp $1/usr/lib/syslinux/bios/mboot.c32 $2
-		cp $1/usr/lib/syslinux/bios/hdt.c32 $2
-		cp $1/usr/lib/syslinux/bios/chain.c32 $2
-		cp $1/usr/lib/syslinux/bios/libcom32.c32 $2
-		cp $1/usr/lib/syslinux/bios/libmenu.c32 $2
-		cp $1/usr/lib/syslinux/bios/libutil.c32 $2
-		cp $1/usr/lib/syslinux/bios/libgpl.c32 $2
+		cp $1/usr/lib/syslinux/bios/isolinux.bin $2/
+		cp $1/usr/lib/syslinux/bios/isohdpfx.bin $2/
+		cp $1/usr/lib/syslinux/bios/ldlinux.c32 $2/
+		cp $1/usr/lib/syslinux/bios/gfxboot.c32 $2/
+		cp $1/usr/lib/syslinux/bios/whichsys.c32 $2/
+		cp $1/usr/lib/syslinux/bios/mboot.c32 $2/
+		cp $1/usr/lib/syslinux/bios/hdt.c32 $2/
+		cp $1/usr/lib/syslinux/bios/chain.c32 $2/
+		cp $1/usr/lib/syslinux/bios/libcom32.c32 $2/
+		cp $1/usr/lib/syslinux/bios/libmenu.c32 $2/
+		cp $1/usr/lib/syslinux/bios/libutil.c32 $2/
+		cp $1/usr/lib/syslinux/bios/libgpl.c32 $2/
 	else
-		cp $1/usr/lib/syslinux/isolinux.bin $2
-		cp $1/usr/lib/syslinux/isohdpfx.bin $2
-		cp $1/usr/lib/syslinux/gfxboot.c32 $2
-		cp $1/whichsys.c32 $2
-		cp $1/usr/lib/syslinux/mboot.c32 $2
-		cp $1/usr/lib/syslinux/hdt.c32 $2
-		cp $1/usr/lib/syslinux/chain.c32 $2
+		cp $1/usr/lib/syslinux/isolinux.bin $2/
+		cp $1/usr/lib/syslinux/isohdpfx.bin $2/
+		cp $1/usr/lib/syslinux/gfxboot.c32 $2/
+		cp $1/usr/lib/syslinux/whichsys.c32 $2/
+		cp $1/usr/lib/syslinux/mboot.c32 $2/
+		cp $1/usr/lib/syslinux/hdt.c32 $2/
+		cp $1/usr/lib/syslinux/chain.c32 $2/
 	fi
 }
 
