fec0626318fa0835a626f76389dc3860dd1a04bc
udeved <artoo@manjaro.org>
2015-06-11 21:49:36 +0200

    [util-iso-image] split ou aufs stuff

--- lib/util-iso-image.sh
+++ lib/util-iso-image.sh
@@ -385,46 +385,6 @@ chroot_create(){
 			$@ || die "Failed to retrieve one or more packages!"
 }
 
-# $1: new branch
-aufs_mount_root_image(){
-	msg2 "mount [root-image] on [${1##*/}]"
-	mount -t aufs -o br="$1":${work_dir}/root-image=ro none "$1"
-}
-
-# $1: add branch
-aufs_append_root_image(){
-	msg2 "append [root-image] on [${1##*/}]"
-	mount -t aufs -o remount,append:${work_dir}/root-image=ro none "$1"
-}
-
-# $1: add branch
-aufs_mount_custom_image(){
-	msg2 "mount [${1##*/}] on [${custom}-image]"
-	mount -t aufs -o br="$1":${work_dir}/${custom}-image=ro none "$1"
-}
-
-# $1: del branch
-aufs_remove_image(){
-	if mountpoint -q "$1";then
-		msg2 "unmount ${1##*/}"
-		umount $1
-	fi
-}
-
-# $1: image path
-aufs_clean(){
-	find $1 -name '.wh.*' -delete &> /dev/null
-}
-
-umount_image_handler(){
-	aufs_remove_image "${work_dir}/livecd-image"
-	aufs_remove_image "${work_dir}/${custom}-image"
-	aufs_remove_image "${work_dir}/root-image"
-	aufs_remove_image "${work_dir}/pkgs-image"
-	aufs_remove_image "${work_dir}/lng-image"
-	aufs_remove_image "${work_dir}/boot-image"
-}
-
 # $1: image path
 clean_up_image(){
 	msg2 "Cleaning up [${1##*/}]"
