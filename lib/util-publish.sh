1d8cd813a6715162391ab50bd6497309f152dae0
udeved <artoo@manjaro.org>
2015-11-13 12:33:48 +0100

    [util-publish] upd

--- lib/util-publish.sh
+++ lib/util-publish.sh
@@ -9,27 +9,13 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-remote_login(){
-	#sshpass -f ${remote_pwd} ssh -t ${shell_url} create
-	ssh -t ${shell_url} create
-	cd ${remote_target}/${remote_project}
-}
-
-create_server_tree(){
-	remote_login
-	cd ${type}
-	# need to establish tree
-	mkdir -pv ${dist_release}
+create_release(){
+        ssh ${shell_url} mkdir -pv ${remote_target}/${remote_project}/${iso_edition}/${dist_release}
 }
 
 upload(){
 	msg "Start upload ..."
-	if ${is_sf};then
-		local files=$(ls ${cache_dir_iso})
-		sshpass -f ${remote_pwd} rsync -vP  --progress -e ssh $files ${sf_url}
-	else
-		msg3 "Do something here if not sf"
-	fi
+        rsync -av --progress -e ssh ${src_dir}/ ${sf_url}/${iso_edition}/${dist_release}/${profile}
 	msg "Done upload"
 	msg3 "Time ${FUNCNAME}: $(elapsed_time ${timer_start}) minutes"
 }
