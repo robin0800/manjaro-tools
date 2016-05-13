0383dea0b4dba90cbbbf7e856e2b5ec3bc950d11
Philip <philm@manjaro.org>
2016-05-14 00:35:03 +0200

    [util-iso-boot] use efitools

--- lib/util-iso-boot.sh
+++ lib/util-iso-boot.sh
@@ -59,8 +59,8 @@ gen_boot_image(){
 
 copy_efi_loaders(){
 	msg2 "Copying efi loaders ..."
-	cp $1/usr/lib/prebootloader/PreLoader.efi $2/bootx64.efi
-	cp $1/usr/lib/prebootloader/HashTool.efi $2/
+	cp $1/usr/lib/efitools/PreLoader.efi $2/bootx64.efi
+	cp $1/usr/lib/efitools/HashTool.efi $2/
 	cp $1/usr/lib/systemd/boot/efi/systemd-bootx64.efi $2/loader.efi
 }
 
