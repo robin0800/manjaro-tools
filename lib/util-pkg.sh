67b5032bc5c7100b0e8208dd1be3f50ae88aaa51
udeved <artoo@manjaro.org>
2017-03-05 14:09:44 +0100

    util-pkg: remove unused vars

--- lib/util-pkg.sh
+++ lib/util-pkg.sh
@@ -60,8 +60,6 @@ prepare_conf(){
 }
 
 pkgver_equal() {
-    local left right
-
     if [[ $1 = *-* && $2 = *-* ]]; then
         # if both versions have a pkgrel, then they must be an exact match
         [[ $1 = "$2" ]]
@@ -98,7 +96,7 @@ get_full_version() {
 find_cached_package() {
     local searchdirs=("$PWD" "$PKGDEST") results=()
     local targetname=$1 targetver=$2 targetarch=$3
-    local dir pkg pkgbasename pkgparts name ver rel arch size r results
+    local dir pkg pkgbasename name ver rel arch r results
 
     for dir in "${searchdirs[@]}"; do
         [[ -d $dir ]] || continue
