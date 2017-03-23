98c3b5ddf146a51dccbbe46ab23263e529751102
artoo <udeved@users.noreply.github.com>
2017-03-23 21:15:35 +0100

    Unify grub (#262)
    * initial commit testing unified grub
    
    * buildiso: update to working grub version
    
    * data: remove grub; move to own repo
    
    * buildiso: adopt new grub-theme path

--- lib/util-iso-boot.sh
+++ lib/util-iso-boot.sh
@@ -52,91 +52,70 @@ prepare_boot_extras(){
     cp $1/usr/share/licenses/common/GPL2/license.txt $2/memtest.COPYING
 }
 
-prepare_efiboot_image(){
-    local efi=$1/EFI/miso boot=$2/${iso_name}/boot
-    prepare_dir "${efi}"
-    cp ${boot}/x86_64/vmlinuz ${efi}/vmlinuz.efi
-    cp ${boot}/x86_64/initramfs.img ${efi}/initramfs.img
-    if [[ -f ${boot}/intel_ucode.img ]] ; then
-        cp ${boot}/intel_ucode.img ${efi}/intel_ucode.img
-    fi
-}
-
 vars_to_boot_conf(){
     sed -e "s|@ISO_NAME@|${iso_name}|g" \
         -e "s|@ISO_LABEL@|${iso_label}|g" \
         -e "s|@DIST_NAME@|${dist_name}|g" \
         -e "s|@ARCH@|${target_arch}|g" \
-        -e "s|@DRV@|$2|g" \
-        -e "s|@SWITCH@|$3|g" \
-        -e "s|@BOOT_ARGS@||g" \
         -i $1
 }
 
-prepare_efi_loader(){
-    local efi_data=$1/usr/share/efi-utils efi=$2/EFI/boot
-    msg2 "Preparing efi loaders ..."
-    prepare_dir "${efi}"
-    cp $1/usr/share/efitools/efi/PreLoader.efi ${efi}/bootx64.efi
-    cp $1/usr/share/efitools/efi/HashTool.efi ${efi}
-    cp ${efi_data}/gummibootx64.efi ${efi}/loader.efi
-    cp ${efi_data}/shellx64_v{1,2}.efi $2/EFI
+prepare_grub(){
+    local src=i386-pc app='core.img' grub=$2/boot/grub efi=$2/efi/boot \
+        data=$1/usr/share/grub lib=$1/usr/lib/grub
+    
+    prepare_dir ${grub}/${src}
+    
+    cp ${data}/cfg/*.cfg ${grub}
+    
+    vars_to_boot_conf "${grub}/grub.cfg"
+    
+    cp ${lib}/${src}/* ${grub}/${src}
+    
+    msg2 "Building %s ..." "${app}"
+    
+    local mods=(iso9660 normal extcmd boot bufio crypto gettext terminal multiboot configfile linux linux16)
+     
+    grub-mkimage -d ${grub}/${src} -o ${grub}/${src}/core.img -O ${src} -p /boot/grub biosdisk ${mods[@]}
 
-    local entries=$2/loader/entries
-    msg2 "Preparing efi loader config ..."
-    prepare_dir "${entries}"
+    cat ${grub}/${src}/cdboot.img ${grub}/${src}/core.img > ${grub}/${src}/eltorito.img
+    
+    case ${target_arch} in 
+        'i686') 
+            src=i386-efi 
+            app=bootia32.efi
+        ;;
+        'x86_64')
+            src=x86_64-efi
+            app=bootx64.efi
+        ;;
+    esac
+    
+    prepare_dir ${efi}
+    prepare_dir ${grub}/${src}
+    
+    cp ${lib}/${src}/* ${grub}/${src}
+    
+    msg2 "Building %s ..." "${app}"
 
-    cp ${efi_data}/loader.conf $2/loader/loader.conf
-    vars_to_boot_conf $2/loader/loader.conf
-    cp ${efi_data}/uefi-shell-v{1,2}-x86_64.conf ${entries}
-
-    local label='free' switch="no"
-    cp ${efi_data}/entry-x86_64-$3.conf ${entries}/${iso_name}-x86_64.conf
-    vars_to_boot_conf "${entries}/${iso_name}-x86_64.conf" "$label" "$switch"
-    if ${nonfree_mhwd};then
-        label='nonfree' switch="yes"
-        cp ${efi_data}/entry-x86_64-$3.conf ${entries}/${iso_name}-x86_64-nonfree.conf
-        vars_to_boot_conf "${entries}/${iso_name}-x86_64-nonfree.conf" "$label" "$switch"
-    fi
-}
-
-check_syslinux_select(){
-    local boot=${iso_root}/${iso_name}/boot
-    if [[ ! -f ${boot}/x86_64/vmlinuz ]] ; then
-        msg2 "Configuring syslinux for i686 architecture only ..."
-        sed -e "s/select.cfg/i686_inc.cfg/g" -i "$1/miso.cfg"
-    fi
-}
-
-check_syslinux_nonfree(){
-    msg2 "Configuring syslinux menu ..."
-    sed -e "/LABEL nonfree/,/^$/d" -i "$1/miso_sys_i686.cfg"
-    sed -e "/LABEL nonfree/,/^$/d" -i "$1/miso_sys_x86_64.cfg"
-    sed -e "/nonfree/ d" -i $1/syslinux.msg
-}
-
-prepare_isolinux(){
-    local syslinux=$1/usr/lib/syslinux/bios
-    msg2 "Copying isolinux binaries ..."
-    cp ${syslinux}/{{isolinux,isohdpfx}.bin,ldlinux.c32} $2
-    msg2 "Copying isolinux.cfg ..."
-    cp $1/usr/share/syslinux/isolinux/isolinux.cfg $2
-    vars_to_boot_conf "$2/isolinux.cfg"
-}
-
-prepare_syslinux(){
-    local syslinux=$1/usr/lib/syslinux/bios
-    msg2 "Copying syslinux binaries ..."
-    cp ${syslinux}/{*.c32,lpxelinux.0,memdisk} $2
-    msg2 "Copying syslinux theme ..."
-    syslinux=$1/usr/share/syslinux/theme
-    cp ${syslinux}/* $2
-    for conf in $2/*.cfg; do
-        vars_to_boot_conf "${conf}"
-    done
-    # Check for dual-arch
-    check_syslinux_select "$2"
-    if ! ${nonfree_mhwd};then
-        check_syslinux_nonfree "$2"
-    fi
+    grub-mkimage -d ${grub}/${src} -o ${efi}/${app} -O ${src} -p /boot/grub ${mods[@]} 
+    
+    prepare_dir ${grub}/themes
+    cp -r ${data}/themes/${iso_name}-live ${grub}/themes/
+    cp ${data}/unicode.pf2 ${grub}
+    cp -r ${data}/{locales,tz} ${grub}
+    
+    local size=8M mnt="${mnt_dir}/efiboot" img="$2/efi.img"
+    msg2 "Creating fat image of %s ..." "${size}"
+    truncate -s ${size} "${img}"
+    mkfs.fat -n MISO_EFI "${img}" &>/dev/null
+    mkdir -p "${mnt}"
+    mount_img "${img}" "${mnt}"
+    
+    prepare_dir ${mnt}/efi/boot
+    
+    msg2 "Building %s ..." "${app}"
+    grub-mkimage -d ${grub}/${src} -o ${mnt}/efi/boot/${app} -O ${src} -p /boot/grub ${mods[@]}
+    
+    umount_img "${mnt}"
 }
