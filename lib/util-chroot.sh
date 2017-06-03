ea0df5a966fbe35e8069f4ae4d961f3e530606ed
udeved <artoo@manjaro.org>
2017-06-03 20:56:57 +0200

    mkchroot, basestrap: fixes, still to solve the mirrors.json issue

--- lib/util-chroot.sh
+++ lib/util-chroot.sh
@@ -20,7 +20,7 @@ copy_keyring(){
 
 create_min_fs(){
     msg "Creating install root at %s" "$1"
-    mkdir -m 0755 -p $1/var/{cache/pacman/pkg,lib/pacman,log} $1/{dev,run,etc}
+    mkdir -m 0755 -p $1/var/{cache/pacman/pkg,lib/{pacman,log,pacman-mirrors}} $1/{dev,run,etc/pacman.d}
     mkdir -m 1777 -p $1/tmp
     mkdir -m 0555 -p $1/{sys,proc}
 }
