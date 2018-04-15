e6ae6aaa3f76cae4a5fb6454ae0307edfff02f84
Bernhard Landauer <oberon@manjaro.org>
2018-04-15 11:37:02 +0200

    fix torrent seed url

--- lib/util-publish.sh
+++ lib/util-publish.sh
@@ -27,7 +27,7 @@ make_torrent(){
 
     if [[ -n $(find ${src_dir} -type f -name "*.iso") ]]; then
         for iso in $(ls ${src_dir}/*.iso);do
-            local seed=${host}/projects/${project}/storage/${profile}/${iso##*/}
+            local seed=${host}/projects/${project}/storage/${profile}/${dist_release}/${iso##*/}
             local mktorrent_args=(-c "${torrent_meta}" -p -l ${piece_size} -a ${tracker_url} -w $(gen_webseed ${seed}))
             ${verbose} && mktorrent_args+=(-v)
             msg2 "Creating (%s) ..." "${iso##*/}.torrent"
