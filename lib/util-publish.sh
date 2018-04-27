1262b50b718da8cc23f146a41d539d49a2a356df
Bernhard Landauer <oberon@manjaro.org>
2018-04-27 11:19:36 +0200

    [util-publish] webseed use shorturl

--- lib/util-publish.sh
+++ lib/util-publish.sh
@@ -20,7 +20,7 @@ make_torrent(){
 
     if [[ -n $(find ${src_dir} -type f -name "*.iso") ]]; then
         for iso in $(ls ${src_dir}/*.iso); do
-            local seed=https://${host}/projects/${project}/storage/${profile}/${dist_release}/${iso##*/}
+            local seed=https://${host}/dl/${project}/${iso##*/}
             local mktorrent_args=(-c "${torrent_meta}" -p -l ${piece_size} -a ${tracker_url} -w ${seed})
             ${verbose} && mktorrent_args+=(-v)
             msg2 "Creating (%s) ..." "${iso##*/}.torrent"
