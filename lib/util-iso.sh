331ed1965931c416816a32b6f76a5d287dc9a184
Stefano <stefano@manjaro.org>
2019-11-14 23:17:30 +0100

    zstd cosmetic support
    Signed-off-by: Stefano <stefano@manjaro.org>

--- lib/util-iso.sh
+++ lib/util-iso.sh
@@ -139,7 +139,7 @@ make_sfs() {
 
     local highcomp="-b 256K -Xbcj x86"
    # local comp='xz'
-    [[ "${iso_compression}" != "xz" ]] && highcomp=""
+    [[ "${iso_compression}" != "xz" ]] || [[ "${iso_compression}" != "zstd" ]] && highcomp=""
 
     if [[ "${name}" == "mhwdfs" && ${used_kernel} < "4" ]]; then
         mksfs_args+=(-comp lz4)
