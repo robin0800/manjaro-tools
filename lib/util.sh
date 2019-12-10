4ac9dc3192ba00e3df0c8c819c1fb29da83553fe
Bernhard Landauer <oberon@manjaro.org>
2019-12-10 11:29:59 +0100

    > LTS linux54

--- lib/util.sh
+++ lib/util.sh
@@ -315,7 +315,7 @@ init_buildiso(){
 
     iso_label=$(get_iso_label "${dist_branding}${dist_release//.}")
 
-    [[ -z ${kernel} ]] && kernel="linux53"
+    [[ -z ${kernel} ]] && kernel="linux54"
     
     load_run_dir "${profile_repo}"
     
