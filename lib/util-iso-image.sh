699d178f527151dfa9452c3d724e0fcb9d8a616a
Philip Müller <philm@manjaro.org>
2019-12-29 09:50:29 +0100

    [util-iso-image] fix copy_from_cache

--- lib/util-iso-image.sh
+++ lib/util-iso-image.sh
@@ -352,8 +352,7 @@ copy_from_cache(){
         -B "${build_mirror}/${target_branch}" \
         "$1" \
         pacman -v -Sp $2 --noconfirm > "$list"
-    sed -ni '/.pkg.tar.xz/p' "$list"
-    sed -ni '/.pkg.tar.zst/p' "$list"
+    sed -ni '/pkg.tar/p' "$list"
     sed -i "s/.*\///" "$list"
 
     msg2 "Copying mhwd package cache ..."
