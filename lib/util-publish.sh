6d1bb50b444a2f3857245ed8c1b149a450a2a527
Bernhard Landauer <bernhard@manjaro.org>
2020-03-01 09:49:06 +0100

    typo

--- lib/util-publish.sh
+++ lib/util-publish.sh
@@ -190,7 +190,7 @@ sync_latest_php(){
     msg2 "Uploading php redirector ..."
     local filename=".latest.php"
     local php="latest.php"
-    [[ ${MINIMAL} == "yes" ]] && filename=".latest.php" && php="latest-minimal.php"
+    [[ ${MINIMAL} == "yes" ]] && filename=".latest-minimal.php" && php="latest-minimal.php"
     chmod g+w "${src_dir}/${filename}"
     scp -p "${src_dir}/.${php}" "${webshell}/${htdocs}/${php}"
 }
